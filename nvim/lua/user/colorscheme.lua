588b52cdd89abd338f0b9467b462f71e2384ef55
knipferrc <knipferrc@gmail.com>
2022-08-21 08:22:27 -0400

    kitty and nvim updates

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
 vim.o.background = 'dark'
-vim.g.gruvbox_material_background = 'hard'
-vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_enable_italic = 1
-vim.cmd("colorscheme gruvbox-material")
+vim.g.everforest_background = 'hard'
+vim.g.everforest_better_performance = 1
+vim.g.everforest_enable_italic = 1
+vim.cmd("colorscheme everforest")
