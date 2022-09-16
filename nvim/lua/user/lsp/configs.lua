28c2f080f4f5a0e02449dba637971358f41c9fac
knipferrc <knipferrc@gmail.com>
2022-09-16 12:59:31 -0400

    chore: move mason to own file

--- nvim/lua/user/lsp/configs.lua
+++ nvim/lua/user/lsp/configs.lua
@@ -1,10 +1,3 @@
-local status_ok, mason = pcall(require, "mason")
-if not status_ok then
-  return
-end
-
-mason.setup()
-
 local ok, mason_lsp_config = pcall(require, "mason-lspconfig")
 if not ok then
   return
