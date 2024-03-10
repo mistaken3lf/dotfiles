0da4169aabac0057db8dddb03cd8fc4dbd81e3f3
mistakenelf <tyler.knipfer@icloud.com>
2024-03-10 13:17:28 -0400

    feat: add bufferline

--- nvim/init.lua
+++ nvim/init.lua
@@ -360,4 +360,61 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		event = "BufReadPre",
+		version = "*",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					mode = "buffers",
+					themable = true,
+					close_command = "bdelete",
+					right_mouse_command = "bdelete",
+					left_mouse_command = "buffer %d",
+					middle_mouse_command = nil,
+					indicator = {
+						icon = "▎",
+						style = "icon",
+					},
+					buffer_close_icon = "X",
+					modified_icon = "●",
+					close_icon = "X",
+					left_trunc_marker = "",
+					right_trunc_marker = "",
+					max_name_length = 18,
+					max_prefix_length = 15,
+					truncate_names = true,
+					tab_size = 18,
+					diagnostics = "nvim_lsp",
+					offsets = {
+						{
+							filetype = "NvimTree",
+							text = "File Explorer",
+							text_align = "left",
+							separator = true,
+						},
+					},
+					show_buffer_icons = true,
+					show_buffer_close_icons = true,
+					show_close_icon = true,
+					show_tab_indicators = false,
+					show_duplicate_prefix = false,
+					duplicates_across_groups = true,
+					persist_buffer_sort = true,
+					move_wraps_at_ends = false,
+					separator_style = "slant",
+					enforce_regular_tabs = false,
+					always_show_bufferline = true,
+					hover = {
+						enabled = true,
+						delay = 200,
+						reveal = { "close" },
+					},
+					sort_by = "insert_after_current",
+				},
+			})
+		end,
+	},
 })
